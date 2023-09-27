d1517f0bfafebd7d08b8cba4e0ba2a8ea1bed81d
nrLucas <lucasnrx@gmail.com>
2023-09-26 23:45:19 -0300

    basic version

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, Button } from "react-native";
+import * as tf from "@tensorflow/tfjs";
+import { decodeJpeg } from "@tensorflow/tfjs-react-native";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import * as jpeg from "jpeg-js";
 
-export default function App() {
+const App = () => {
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [result, setResult] = useState("");
+  const [pickedImage, setPickedImage] = useState("");
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setPickedImage(result.uri);
+    }
+  };
+
+  const classifyUsingMobilenet = async () => {
+    try {
+      // Load mobilenet.
+      await tf.ready();
+      const model = await mobilenet.load();
+      setIsTfReady(true);
+      console.log("starting inference with picked image: " + pickedImage);
+
+      // Convert image to tensor
+      const imgB64 = await FileSystem.readAsStringAsync(pickedImage, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const imgBuffer = tf.util.encodeString(imgB64, "base64").buffer;
+      const raw = new Uint8Array(imgBuffer);
+      const imageTensor = decodeJpeg(raw);
+      // Classify the tensor and show the result
+      const prediction = await model.classify(imageTensor);
+      if (prediction && prediction.length > 0) {
+        setResult(
+          `${prediction[0].className} (${prediction[0].probability.toFixed(3)})`
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    classifyUsingMobilenet();
+  }, [pickedImage]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Image
+        source={{ uri: pickedImage }}
+        style={{ width: 200, height: 200, margin: 40 }}
+      />
+      {isTfReady && <Button title="Pick an image" onPress={pickImage} />}
+      <View style={{ width: "100%", height: 20 }} />
+      {!isTfReady && <Text>Loading TFJS model...</Text>}
+      {isTfReady && result === "" && <Text>Pick an image to classify!</Text>}
+      {result !== "" && <Text>{result}</Text>}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
